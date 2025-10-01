5af7c9bac85401d23e28990120677009cdf1dece
JhonatanAlva <84637610+JhonatanAlva@users.noreply.github.com>
2025-09-30 21:28:14 -0600

    ruta modificada

--- src/app/api/empleados/[id]/route.ts
+++ src/app/api/empleados/[id]/route.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(req: Request, context: any) {
   try {
+
+    const id = context?.params?.id as string
+
     const baseUrl =
       process.env.NEXT_PUBLIC_API_BASE_URL_EMPLOYEE || 'http://localhost:8091'
 
-    const cookieStore = await cookies()
+    // cookies() no es async, no uses await
+    const cookieStore = cookies()
     const tokenCookie = cookieStore.get(process.env.AUTH_COOKIE_NAME || 'one21_token')
 
     if (!tokenCookie?.value) {
@@ -19,8 +20,7 @@ export async function PUT(
     const token = tokenCookie.value
     const body = await req.json()
 
-
-    const res = await fetch(`${baseUrl}/employees/${params.id}`, {
+    const res = await fetch(`${baseUrl}/employees/${id}`, {
       method: 'PUT',
       headers: {
         Authorization: `Bearer ${token}`,
