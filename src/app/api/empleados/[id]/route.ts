14666cfb2ddf30b13a3ac5792c44da7ede714b17
JhonatanAlva <84637610+JhonatanAlva@users.noreply.github.com>
2025-09-30 21:32:36 -0600

    ruta modificada

--- src/app/api/empleados/[id]/route.ts
+++ src/app/api/empleados/[id]/route.ts
@@ -1,15 +1,13 @@
 import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
-export async function PUT(req: Request, context: any) {
+export async function PATCH(req: Request, context: any) {
   try {
-
     const id = context?.params?.id as string
 
     const baseUrl =
       process.env.NEXT_PUBLIC_API_BASE_URL_EMPLOYEE || 'http://localhost:8091'
 
-    // cookies() no es async, no uses await
     const cookieStore = cookies()
     const tokenCookie = cookieStore.get(process.env.AUTH_COOKIE_NAME || 'one21_token')
 
@@ -20,8 +18,8 @@ export async function PUT(req: Request, context: any) {
     const token = tokenCookie.value
     const body = await req.json()
 
-    const res = await fetch(`${baseUrl}/employees/${id}`, {
-      method: 'PUT',
+    const res = await fetch(`${baseUrl}/employees/status/${id}`, {
+      method: 'PATCH',
       headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json'
@@ -31,9 +29,9 @@ export async function PUT(req: Request, context: any) {
 
     if (!res.ok) {
       const msg = await res.text()
-
+      
       return NextResponse.json(
-        { message: msg || 'Error al actualizar empleado' },
+        { message: msg || 'Error al actualizar estado del empleado' },
         { status: res.status }
       )
     }
@@ -42,10 +40,10 @@ export async function PUT(req: Request, context: any) {
 
     return NextResponse.json(data, { status: 200 })
   } catch (err) {
-    console.error('❌ Error en /api/empleados/[id]:', err)
-    
+    console.error('❌ Error en /api/empleados/status/[id]:', err)
+
     return NextResponse.json(
-      { message: 'Error interno al actualizar empleado' },
+      { message: 'Error interno al cambiar estado del empleado' },
       { status: 500 }
     )
   }
