3663f820f9dbfb26c02aeaf0a8194db29593f576
JeffMolina <84147110+JeffersonMolina-art@users.noreply.github.com>
2025-10-11 22:35:46 -0600

    Actualizar la configuración del menú para ajustar los roles de acceso y simplificar la lógica de permisos.

--- src/configs/menuConfig.ts
+++ src/configs/menuConfig.ts
@@ -7,7 +7,7 @@ export const menuConfig = [
     label: 'Inicio',
     icon: 'tabler-smart-home',
     href: '/inicio',
-    roles: ['app-admin', 'patient', 'default-roles-master', 'uma_authorization']
+    roles: ['app-admin', 'patient', 'default-roles-master', 'uma_authorization', 'client']
   },
   {
     type: 'section',
@@ -18,14 +18,14 @@ export const menuConfig = [
         label: 'Empleados',
         href: '/Empleados',
         icon: 'tabler-user',
-        roles: ['app-admin', 'uma_authorization']
+        roles: ['app-admin']
       },
       {
         type: 'item',
         label: 'Personas',
         href: '/personas',
         icon: 'tabler-users',
-        roles: ['app-admin', 'patient', 'uma_authorization']
+        roles: ['app-admin']
       },
       {
         type: 'submenu',
@@ -33,12 +33,12 @@ export const menuConfig = [
         icon: 'tabler-box',
         roles: ['app-admin', 'uma_authorization'],
         children: [
-          { type: 'item', label: 'Crear Ticket', href: '/ticket/crear', roles: ['app-admin', 'uma_authorization'] },
-          { type: 'item', label: 'Asignar Tickets', href: '/ticket/asignar', roles: ['app-admin', 'uma_authorization'] },
-          { type: 'item', label: 'Seguimiento de Tickets', href: '/kanban', roles: ['app-admin', 'uma_authorization'] },
-          { type: 'item', label: 'Prioridades', href: '/prioridades', roles: ['app-admin', 'uma_authorization'] },
-          { type: 'item', label: 'Categorías', href: '/categorias', roles: ['app-admin', 'uma_authorization'] },
-          { type: 'item', label: 'Estados', href: '/status', roles: ['app-admin', 'uma_authorization'] },
+          { type: 'item', label: 'Crear Ticket', href: '/ticket/crear', roles: ['app-admin', 'client', 'employee'] },
+          { type: 'item', label: 'Asignar Tickets', href: '/ticket/asignar', roles: ['app-admin'] },
+          { type: 'item', label: 'Seguimiento de Tickets', href: '/kanban', roles: ['app-admin', 'employee'] },
+          { type: 'item', label: 'Prioridades', href: '/prioridades', roles: ['app-admin'] },
+          { type: 'item', label: 'Categorías', href: '/categorias', roles: ['app-admin'] },
+          { type: 'item', label: 'Estados', href: '/status', roles: ['app-admin'] },
         ]
       },
       {
@@ -47,12 +47,12 @@ export const menuConfig = [
         icon: 'tabler-settings',
         roles: ['app-admin', 'uma_authorization'],
         children: [
-          { type: 'item', label: 'Paises', href: '/countries', roles: ['app-admin', 'uma_authorization'] },
+          { type: 'item', label: 'Paises', href: '/countries', roles: ['app-admin'] },
           { type: 'item', label: 'Roles', href: '/roles', roles: ['app-admin'] },
           { type: 'item', label: 'Puestos de trabajo', href: '/job_position', roles: ['app-admin'] },
           { type: 'item', label: 'Departamento de trabajo', href: '/employee_departaments', roles: ['app-admin'] },
-          { type: 'item', label: 'Departamentos', href: '/departments', roles: ['app-admin', 'uma_authorization'] },
-          { type: 'item', label: 'Municipios', href: '/municipalities', roles: ['app-admin', 'uma_authorization'] },
+          { type: 'item', label: 'Departamentos', href: '/departments', roles: ['app-admin'] },
+          { type: 'item', label: 'Municipios', href: '/municipalities', roles: ['app-admin'] },
         ]
       }
     ]
